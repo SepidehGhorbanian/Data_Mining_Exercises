356b9cc16acbb96f4b5e6e64de5dfab95aa79b31
Unisepp <106227374+Unisepp@users.noreply.github.com>
2022-09-02 18:49:14 +0430

    Update K_means.py
    adding wcss

--- K_Means_Exercise/K_means.py
+++ K_Means_Exercise/K_means.py
@@ -13,15 +13,27 @@ from sklearn.preprocessing import StandardScaler
 # Preparing the data
 data = pd.read_csv('iris.csv')
 y = data['variety']
-x=data.drop('variety' , axis = 1 )
+x=data.drop('variety' , axis = 1 ) #deleting the labels
 x= pd.DataFrame(StandardScaler().fit_transform(x) , columns = x.columns)
 # print(x.head())
+
+# Finding the best k
+wcss=[]
+for i in range(1,11):
+    opt_km = KMeans(n_clusters = i , init='k-means++' , n_init = 10 , random_state=1)
+    opt_km.fit(x)
+    wcss.append(opt_km.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+# The best k is 3
 
 # Setting up k-means
 km = KMeans(n_clusters = 3 , init='k-means++' , n_init = 10 , random_state=1)
 # Fitting the model to the data
 km.fit(x)
-
 
 # Visualizing
 sns.scatterplot(data = x , x = 'sepal.length' , y='sepal.width' , hue = y, style = km.labels_)
