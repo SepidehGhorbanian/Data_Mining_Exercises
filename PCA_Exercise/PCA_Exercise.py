024df766e24b5ce0264cf02ac1c647470ae619e0
Unisepp <106227374+Unisepp@users.noreply.github.com>
2022-09-13 11:45:56 +0430

    Update PCA_Exercise.py

--- PCA_Exercise/PCA_Exercise.py
+++ PCA_Exercise/PCA_Exercise.py
@@ -17,5 +17,5 @@ print(data.head())
 pca = PCA()
 data_tf = pca.fit_transform(data)
 vis1=plt.plot(pca.explained_variance_ratio_)  # We can see that we have a 2 component solution
-
+plt.show()
 vis2=sns.scatterplot(x = data_tf[: , 0] , y = data_tf[: , 1]) # We can see a clean seperation
